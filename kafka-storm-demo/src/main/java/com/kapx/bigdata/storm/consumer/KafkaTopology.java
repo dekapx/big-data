0c42a64f820270f353346d4cf210968d7542bab0
KAPIL <xkapil@gmail.com>
2015-12-21 10:17:20 +0000

    code refactoring

--- kafka-storm-demo/src/main/java/com/kapx/bigdata/storm/consumer/KafkaTopology.java
+++ kafka-storm-demo/src/main/java/com/kapx/bigdata/storm/consumer/KafkaTopology.java
@@ -1,5 +1,7 @@
 package com.kapx.bigdata.storm.consumer;
 
+import static com.kapx.bigdata.common.util.CommonConstants.FIELD_EMAIL;
+
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.spout.SchemeAsMultiScheme;
@@ -14,13 +16,19 @@ import storm.kafka.ZkHosts;
 
 public class KafkaTopology {
 	private static final int ONE_MINUTE = 60000;
+	private static final String BROKER_HOST_URL = "localhost:2181";
 	private static final String KAFKA_TOPIC = "test-topic";
 	private static final String APPLICATION_ROOT = "/kafka-storm-demo";
 	private static final String TOPOLOGY_NAME = "kafka-storm-topology";
 	private static final String ID = "some-id";
 
+	private static final String KAFKA_SPOUT = "kafka-spout";
+	private static final String COMMIT_FEED_BOLT = "commit-feed-bolt";
+	private static final String EMAIL_EXTRACTOR_BOLT = "email-extractor-bolt";
+	private static final String EMAIL_COUNTER_BOLT = "email-counter-bolt";
+
 	public static void main(String args[]) {
-		final BrokerHosts brokerHosts = new ZkHosts("localhost:2181");
+		final BrokerHosts brokerHosts = new ZkHosts(BROKER_HOST_URL);
 		final SpoutConfig spoutConf = new SpoutConfig(brokerHosts, KAFKA_TOPIC, APPLICATION_ROOT, ID);
 		spoutConf.scheme = new SchemeAsMultiScheme(new StringScheme());
 		final KafkaSpout kafkaSpout = new KafkaSpout(spoutConf);
@@ -39,10 +47,10 @@ public class KafkaTopology {
 
 	private static TopologyBuilder buildTopology(final KafkaSpout kafkaSpout) {
 		final TopologyBuilder builder = new TopologyBuilder();
-		builder.setSpout("kafka-spout", kafkaSpout, 1);
-		builder.setBolt("commit-feed-bolt", new CommitFeedBolt()).shuffleGrouping("kafka-spout");
-		builder.setBolt("email-extractor", new EmailExtractorBolt()).shuffleGrouping("commit-feed-bolt");
-		builder.setBolt("email-counter", new EmailCounterBolt()).fieldsGrouping("email-extractor", new Fields("email"));
+		builder.setSpout(KAFKA_SPOUT, kafkaSpout, 1);
+		builder.setBolt(COMMIT_FEED_BOLT, new CommitFeedBolt()).shuffleGrouping(KAFKA_SPOUT);
+		builder.setBolt(EMAIL_EXTRACTOR_BOLT, new EmailExtractorBolt()).shuffleGrouping(COMMIT_FEED_BOLT);
+		builder.setBolt(EMAIL_COUNTER_BOLT, new EmailCounterBolt()).fieldsGrouping(EMAIL_EXTRACTOR_BOLT, new Fields(FIELD_EMAIL));
 		return builder;
 	}
 
